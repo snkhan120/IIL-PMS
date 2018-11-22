3e957fc797e765598685544375d5ed5c212a3a90
Muhammad Ahsan Khan <ahsan.khan@ismailindustries.com>
2018-11-22 11:53:06 +0500

    Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Alert  } from 'reactstrap';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Alert color="success">
+        <h4 className="alert-heading">Well done!</h4>
+        <p>
+          Aww yeah, you successfully read this important alert message. This example text is going
+          to run a bit longer so that you can see how spacing within an alert works with this kind
+          of content.
+        </p>
+        <hr />
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things nice and tidy.
+        </p>
+      </Alert>
       </div>
     );
   }
