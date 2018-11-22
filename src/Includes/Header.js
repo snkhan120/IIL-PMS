66a2a1a001795d46fdef3665232ec70ad69807c9
Muhammad Ahsan Khan <ahsan.khan@ismailindustries.com>
2018-11-22 12:12:49 +0500

    Include Headers

---
+++ src/Includes/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import NavBar from "./NavBar"
+
+class Header extends Component {
+  
+    render() {
+      return (
+        <NavBar />
+      );
+    }
+  }
+
+export default Header;
